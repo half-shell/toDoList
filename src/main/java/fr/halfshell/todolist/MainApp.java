ce6421beac4cf4fce77a6a5950bb7c9a1e261652
half-shell <michel.maubert.pro@gmail.com>
2016-03-16 19:07:10 +0100

    Removed rootLayout for consistency

--- src/main/java/fr/halfshell/todolist/MainApp.java
+++ src/main/java/fr/halfshell/todolist/MainApp.java
@@ -7,16 +7,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class MainApp extends Application {
 
-
     private Stage primaryStage;
-    private BorderPane rootLayout;
 
     public static void main(String[] args) {
         launch(args);
@@ -27,37 +24,21 @@ public class MainApp extends Application {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("My Todo list App");
 
-        initRootLayout();
-
         showTodolistOverview();
-    }
-
-    public void initRootLayout() {
-        try {
-            FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/rootLayout.fxml"));
-            rootLayout = (BorderPane) loader.load();
-            System.out.println(loader.getLocation());
-
-            Scene scene = new Scene(rootLayout);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public void showTodolistOverview() {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(MainApp.class.getResource("view/TodolistOverview.fxml"));
-            AnchorPane todolist = (AnchorPane) loader.load();
+            AnchorPane todolist = loader.load();
 
-            rootLayout.setCenter(todolist);
+            Scene scene = new Scene(todolist);
+
+            primaryStage.setScene(scene);
+            primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
-
