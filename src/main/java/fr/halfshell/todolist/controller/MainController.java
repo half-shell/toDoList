d1442aa8aaa4759d7ebbbbe1523a80d54e5e7e55
half-shell <michel.maubert.pro@gmail.com>
2016-08-18 18:04:30 +0200

    Ftr: Add the ability to remove a ToDo
    This commit showed how the ListView node is actually pretty restrictive.
    So the way to delete a ToDo is a change from the existing "Add" Button into a "Del" one.

--- src/main/java/fr/halfshell/todolist/controller/MainController.java
+++ src/main/java/fr/halfshell/todolist/controller/MainController.java
@@ -11,18 +11,32 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import fr.halfshell.todolist.model.ToDo;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * Created by John on 28/01/2016.
  */
 
 public class MainController implements Initializable {
+
+    @FXML // The main anchorPane
+    private AnchorPane toDoAnchorPane;
     @FXML // List of ToDoList
     private ListView<ToDo> toDoListView;
     @FXML // TodoText to Add
     private TextField textFieldToDo;
+    @FXML // Action button to add & delete
+    private Button buttonAddToDo;
+
     // Observable list for auto-update in the list view
     private static final ObservableList<ToDo> toDoObservableList = FXCollections.observableArrayList();
+
+    // this method is launched first
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        // Displaying the ToDos
+        toDoListView.setItems(toDoObservableList);
+        //TODO: Change the ListView for a TableView to let it be more flexible
+    }
 
     @FXML
     public void handleAddToDoAction(){
@@ -30,15 +44,19 @@ public class MainController implements Initializable {
     }
 
     @FXML
+    public void handleListViewItemSelectedAction(){
+        if(!toDoObservableList.isEmpty() & toDoListView.getFocusModel().getFocusedIndex() != -1){
+            buttonAddToDo.setText("Del");
+            buttonAddToDo.setOnAction(event -> rmSelectedTodo(toDoListView.getFocusModel().getFocusedIndex()));
+        }
+    }
+
+    @FXML
+    //TODO: Change this to the onAction event for the textField
     public void onEnter(KeyEvent event){
         if(event.getCode().equals(KeyCode.ENTER)){
             addTextFieldToList();
         }
-    }
-
-    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-        // Displaying the ToDos
-        toDoListView.setItems(toDoObservableList);
     }
 
     private void addTextFieldToList() {
@@ -46,15 +64,18 @@ public class MainController implements Initializable {
             ToDo todo = new ToDo(textFieldToDo.getText());
             toDoObservableList.add(todo);
             textFieldToDo.clear();
-            toDoListView.requestFocus();
+            // Requesting the focus so the textField placeholder appears everytime
+            toDoAnchorPane.requestFocus();
         }
     }
 
-    public static void rmSelectedTodo(int i){
+    public void rmSelectedTodo(int i){
         if(!toDoObservableList.isEmpty()){
-            System.out.println("Todo " + i +": "+toDoObservableList.get(i) +" removed.");
             toDoObservableList.remove(i);
+            buttonAddToDo.setText("Add");
+            buttonAddToDo.setOnAction(event -> addTextFieldToList());
+            // Reset focus
+            toDoAnchorPane.requestFocus();
         }
-        else System.out.println("Todo list is empty.");
     }
 }
